69273b13e22b884fa379da0e08f2b9224eebc613
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-04 20:10:02 -0700

    added additional input fields

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -10,12 +10,12 @@ var form = d3.select("#form");
 //  select output table
 var ufoTable = d3.select("#ufo-results");
 
-// event handlers 
-button.on("click", makeTable);
-form.on("submit", makeTable);
+function match_data(inputs) {
+
+}
 
 // event handler function for the form
-function makeTable(e) {
+function makeTable(d) {
 
     // Prevent the page from refreshing
     // currently only working with button, not pressing enter in input box
@@ -23,10 +23,11 @@ function makeTable(e) {
     d3.event.preventDefault();
 
     // Get the value property of the input element
-    var inputValue = d3.select("#datetime").property("value");
+    var date = d3.select("#datetime").property("value");
+
     //console.log(inputValue);
     // Use the form input to filter the data by date
-    var matches = ufoData.filter(function(ufo) {return ufo.datetime === inputValue;});
+    var matches = ufoData.filter(function(ufo) {return ufo.datetime === date;});
     //console.log(matches);
     //clear info
     ufoTable.html("");
@@ -42,5 +43,10 @@ function makeTable(e) {
         row.append("td").text(`${match.comments}`);
     });
 
-    dttext.text("");
-  };
+    //dttext.text("");
+    
+};
+
+// event handlers 
+button.on("click", makeTable);
+form.on("submit", makeTable);
