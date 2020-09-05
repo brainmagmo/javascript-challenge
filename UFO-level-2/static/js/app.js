024e058506f0664ea7c980abdc5ecae132e7538f
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-04 21:09:21 -0700

    solved multi-filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -11,7 +11,31 @@ var form = d3.select("#form");
 var ufoTable = d3.select("#ufo-results");
 
 function match_data(inputs) {
+    var matches = ufoData;
+    if (inputs.date) {
+        matches = matches.filter(ufo => ufo.datetime == inputs.date);
+    }
+    if (inputs.city) {
+        matches = matches.filter(ufo => ufo.city.includes(inputs.city.toLowerCase()));
+    }
+    if (inputs.state) {
+        matches = matches.filter(ufo => ufo.state.includes(inputs.state.toLowerCase()));
+    }
+    if (inputs.country) {
+        matches = matches.filter(ufo => ufo.country.includes(inputs.country.toLowerCase()));
+    }
+    if (inputs.duration) {
+        matches = matches.filter(ufo => ufo.durationMinutes.includes(inputs.duration));
+    }
+    if (inputs.comments) {
+        matches = matches.filter(ufo => ufo.comments.toLowerCase().includes(inputs.comments.toLowerCase()));
+    }
+    if (inputs.shape) {
+        matches = matches.filter(ufo => ufo.shape.toLowerCase().includes(inputs.shape.toLowerCase()));
+    }
 
+
+    return matches;
 }
 
 // event handler function for the form
@@ -22,12 +46,22 @@ function makeTable(d) {
     //also how to clear input?
     d3.event.preventDefault();
 
-    // Get the value property of the input element
-    var date = d3.select("#datetime").property("value");
+    //collect inputs
+    var inputs = {date:'', city:'', state:'', country:'', shape:'', duration:0, comments:''};
 
-    //console.log(inputValue);
-    // Use the form input to filter the data by date
-    var matches = ufoData.filter(function(ufo) {return ufo.datetime === date;});
+    // Get the value property of the input elements
+    inputs.date = d3.select("#datetime").property("value");
+    inputs.city = d3.select("#city").property("value");
+    inputs.state = d3.select("#state").property("value");
+    inputs.country = d3.select("#country").property("value");
+    inputs.duration = d3.select("#duration").property("value");
+    inputs.comments = d3.select("#comments").property("value");
+    inputs.shape = d3.select('#shape').property("value");
+
+    // Use the form input to filter the data
+    var matches = match_data(inputs);
+
+
     //console.log(matches);
     //clear info
     ufoTable.html("");
